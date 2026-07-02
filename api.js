d1aaf8e2da09cad27dd1e3d29abf75dbd4846f94
uniconsulta <marcosdalleprane2@gmail.com>
2026-07-02 00:27:19 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,11 @@
+const DASHBOARD_URL = "https://consultaedu-dashboard-api.marcosdalleprane2.workers.dev/dashboard";
 
+export async function buscarDashboard() {
+  const resposta = await fetch(`${DASHBOARD_URL}?t=${Date.now()}`);
+
+  if (!resposta.ok) {
+    throw new Error("Erro ao buscar dashboard");
+  }
+
+  return await resposta.json();
+}
