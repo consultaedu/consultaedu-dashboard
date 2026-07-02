e56379e3a92885eaa9cc5b94409153588fa7744d
uniconsulta <marcosdalleprane2@gmail.com>
2026-07-02 00:27:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+import { buscarDashboard } from "./api.js";
+import {
+  atualizarCards,
+  atualizarUltimosChamados,
+  atualizarBarras,
+  atualizarHorario
+} from "./ui.js";
 
+async function carregarDashboard() {
+  try {
+    const dados = await buscarDashboard();
+
+    atualizarCards(dados);
+    atualizarUltimosChamados(dados.ultimos);
+    atualizarBarras("instituicoes", dados.instituicoes);
+    atualizarBarras("problemas", dados.problemas);
+    atualizarHorario();
+
+  } catch (erro) {
+    console.error("Erro ao carregar dashboard:", erro);
+  }
+}
+
+carregarDashboard();
+
+setInterval(carregarDashboard, 5000);
